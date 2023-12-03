297279e5b5c6815d4a4b2838b9f731797212d111
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2023-12-03 13:07:40 +0700

    add search title

--- src/components/modules/SeriesModule/index.tsx
+++ src/components/modules/SeriesModule/index.tsx
@@ -4,30 +4,44 @@ import { SeriesCard } from '@elements'
 import { useApi } from '@hooks'
 import { useEffect, useState } from 'react'
 import { Series } from 'src/components/elements/SeriesCard/interface'
+import { useDebouncedCallback } from 'use-debounce'
 
 export const SeriesModule = () => {
   const { api } = useApi()
   const [series, setSeries] = useState<Series[]>([])
+  const [searchTitle, setSearchTitle] = useState('')
 
   const fetchSeries = async () => {
-    const { response } = await api.get('/series?title=&type=')
+    const { response } = await api.get(`/series?title=${searchTitle}&type=`)
     setSeries(response.data)
-
-    console.log(response)
   }
 
   useEffect(() => {
     fetchSeries()
-  }, [])
+  }, [searchTitle])
+
+  const debouncedSearch = useDebouncedCallback((value) => {
+    setSearchTitle(value)
+  }, 300)
 
   return (
     <div className="flex px-12">
-      <div className="w-1/5"></div>
-      <div className="w-full flex flex-col gap-6">
-        {series.map((value) => (
-          <SeriesCard {...value} key={value.id} />
-        ))}
-      </div>
+      <section className="w-1/5"></section>
+      <section className="w-full flex flex-col gap-6">
+        <div className="w-full flex">
+          <input
+            className="w-1/3 py-2 px-4 rounded-xl bg-platinum text-black outline-folly"
+            type="text"
+            placeholder="Search by title"
+            onChange={(e) => debouncedSearch(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col gap-6">
+          {series.map((value) => (
+            <SeriesCard {...value} key={value.id} />
+          ))}
+        </div>
+      </section>
     </div>
   )
 }
