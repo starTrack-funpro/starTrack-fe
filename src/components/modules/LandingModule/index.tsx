b95a34e6e963655e759875b9349c89a836152533
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2023-12-05 10:57:45 +0700

    add link

--- src/components/modules/LandingModule/index.tsx
+++ src/components/modules/LandingModule/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Button } from '@elements'
+import Link from 'next/link'
 
 export const LandingModule = () => {
   return (
@@ -8,7 +9,9 @@ export const LandingModule = () => {
       <div className="w-1/2"></div>
       <div className="w-1/2 flex flex-col items-center justify-center gap-8">
         <h1 className="text-4xl font-bold">Start Tracking Now!</h1>
-        <Button>Find Your Favorite Series</Button>
+        <Link href="/series">
+          <Button>Find Your Favorite Series</Button>
+        </Link>
       </div>
     </main>
   )
